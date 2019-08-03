6cba8ddfc9f710c4e015cc45b366bdf3f97cc812
Danica Valdez <danicavaldez@Danicas-MacBook-Pro.local>
2019-08-03 11:30:04 -0700

    MVP Completed

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,18 +1,37 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect} from "react";
+import axios from "axios"
+import Card from "./components/Card.js"
+
+import styled from "styled-components"
+import "./App.css";
+
+const StyledDiv = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+`
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
-
-  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+  const [persons, setPersonState] = useState([])
+  
+  useEffect(() => {
+    axios
+      .get("https://swapi.co/api/people/")
+      .then(response => setPersonState(response.data.results))
+      .catch(err => console.error(err))
+  }, [])
+  
+  console.log(persons)
 
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
-    </div>
+      <StyledDiv>
+        {persons.map((person, index) => {
+          return <Card name={person.name} key={index} mass={person.mass} height={person.height} birthday={person.birth_year} eyeColor={person.eye_color} hairColor={person.hair_color}/> 
+        })}
+      </StyledDiv>
+      </div>
   );
 }
 
